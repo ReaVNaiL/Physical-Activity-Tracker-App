b382889f69525e97995f4616e64d26c4819238b1
Samuel Iradukunda <54182358+iradukundas@users.noreply.github.com>
2022-10-16 18:15:43 -0400

    added plotting ability, import button

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -1,33 +1,48 @@
 from tkinter import *
 from tkinter.filedialog import askopenfile
+import pandas as pd
+import matplotlib.pyplot as plt
 '''
 Color Pallet Ref: https://colorhunt.co/palette/f7f7f7eeeeee393e46929aab
 '''
 
 #SETUP
-window = Tk()
-window.geometry("1920x1080")
-window.title("Physical Activity Tracker")
-window.config(bg="#F7F7F7") #background
-window.iconbitmap("./assets/windowIcon.ico") #icon
+root = Tk()
+root.geometry("400x500")
+root.title("Physical Activity Tracker")
+root.config(bg="#F7F7F7") #background
+root.iconbitmap("./assets/windowIcon.ico") #icon
 
-def dataImport():
-	file = askopenfile(parent=window, mode='r', title = "choose file", filetypes =[('Excel Files', '*.xlsm')])
 
+def loadCSV() -> bool:
+	file = askopenfile(parent=root, mode='r', title = "choose file", filetypes =[('Excel Files', '*.csv')])
+	if(file):
+		isLoaded = True
+		df = pd.read_csv(file)
+		df.plot(x="Datetime (UTC)", y = ["Acc magnitude avg"])
+		plt.show()
+	else:
+		isLoaded = False
+	return isLoaded
+		
 #menu
-menubar = Menu(window)
-window.config(menu=menubar)
+menubar = Menu(root)
+root.config(menu=menubar)
 
 #Data Menu
 file_menu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Data", menu=file_menu)
-file_menu.add_command(label="Import", command=dataImport)
+file_menu.add_command(label="Options about Data will be added here", command=loadCSV)
 
 #analysis menu
 timeSeries = Menu(menubar, tearoff = 0)
 menubar.add_cascade(label="Analysis", menu=timeSeries)
 timeSeries.add_command(label="Something")
 
+button = Button(root, text="Import CSV", command = loadCSV)
+button.pack()
 
 
-window.mainloop()
+
+
+root.mainloop()
