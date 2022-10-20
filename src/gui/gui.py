32d77a669aaa98078167998777fdf36ba7917255
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-10-19 23:24:29 -0400

    Refactor Code To Accomodate Startup File

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -2,47 +2,81 @@ from tkinter import *
 from tkinter.filedialog import askopenfile
 import pandas as pd
 import matplotlib.pyplot as plt
+
 '''
 Color Pallet Ref: https://colorhunt.co/palette/f7f7f7eeeeee393e46929aab
 '''
 
-#SETUP
+''' 
+Global Variables Root
+'''
 root = Tk()
-root.geometry("400x500")
-root.title("Physical Activity Tracker")
-root.config(bg="#F7F7F7") #background
-root.iconbitmap("./assets/windowIcon.ico") #icon
 
 
-def loadCSV() -> bool:
-	file = askopenfile(parent=root, mode='r', title = "choose file", filetypes =[('Excel Files', '*.csv')])
-	if(file):
-		isLoaded = True
-		df = pd.read_csv(file)
-		df.plot(x="Datetime (UTC)", y = ["Acc magnitude avg"])
-		plt.show()
-	else:
-		isLoaded = False
-	return isLoaded
-		
-#menu
-menubar = Menu(root)
-root.config(menu=menubar)
-
-#Data Menu
-file_menu = Menu(menubar, tearoff=0)
-menubar.add_cascade(label="Data", menu=file_menu)
-file_menu.add_command(label="Options about Data will be added here", command=loadCSV)
-
-#analysis menu
-timeSeries = Menu(menubar, tearoff = 0)
-menubar.add_cascade(label="Analysis", menu=timeSeries)
-timeSeries.add_command(label="Something")
-
-button = Button(root, text="Import CSV", command = loadCSV)
-button.pack()
+def import_csv() -> bool:
+    file = askopenfile(parent=root, mode='r', title="choose file", filetypes=[
+        ('Excel Files', '*.csv')])
+    if (file):
+        isLoaded = True
+        df = pd.read_csv(file)
+        df.plot(x="Datetime (UTC)", y=["Acc magnitude avg"])
+        plt.show()
+    else:
+        isLoaded = False
+    return isLoaded
 
 
+class MainWindow():
+    """ This will not build, it's just a placeholder """
+    menu_bar = Menu(root)
+
+    def __init__(self, height, width, name, color="#F7F7F7"):
+        self.set_window(height, width, name, color)
+        self.resize(800, 600)
+        
+        # menu
+        root.config(menu=self.menu_bar)
+
+        # Data Menu
+        file_menu = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Data", menu=file_menu)
+        file_menu.add_command(
+            label="Options about Data will be added here", command=import_csv)
+
+        # analysis menu
+        timeSeries = Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Analysis", menu=timeSeries)
+        timeSeries.add_command(label="Something")
+
+        # button = Button(root, text="Import CSV", command=import_csv)
+        # button.pack()
+
+    def set_window(self, height, width, name, color):
+        '''
+        Sets up the window with the given parameters
+        '''
+        self.resize(height, width)
+        root.title(name)
+        root.configure(bg=color)
+
+        # root.title("Physical Activity Tracker")
+        # root.config(bg="#F7F7F7")  # background
+        # root.iconbitmap("./assets/windowIcon.ico") #icon
+
+    def resize(self, height, width):
+        '''
+        Resizes the window
+        '''
+        root.geometry(f"{height}x{width}")
+    
+    def show(self):
+        '''
+        Shows the window
+        '''
+        root.mainloop()
+    
+
+        
 
 
-root.mainloop()
+
--- src/startup.py
+++ src/startup.py
@@ -1,37 +1,20 @@
 # Importing the necessary modules
+from importlib.resources import path
 import sys
 import os
 
 # Path: Physical-Activity-Tracker-App\src\
-from models import *
-from gui import *
+import gui.gui as gui
 import load_csv as lc
-
-class MainWindow():
-    """ This will not build, it's just a placeholder """
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Physical Activity Tracker App")
-        self.resize(800, 600)
-        
-    def show(self):
-        super().show()
-        
-    def setWindowTitle(self, title):
-        super().setWindowTitle(title)
-    
-    def resize(self, width, height):
-        super().resize(width, height)
 
 
 def start_up():
     # This function will be called when the app is started
     
     """ Placeholder for main window """
-    # main_window = MainWindow() 
-
     # Show the main window
-    # main_window.show()
+    program = gui.MainWindow(800, 600, "Physical Activity Tracker")
+    program.show()
     
     csv_sample_path = os.path.join(os.path.dirname(__file__), "../data/20200118/310/summary.csv")
     
