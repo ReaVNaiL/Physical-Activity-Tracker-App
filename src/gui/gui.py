72bd3e862f2c2575d3c06e884142ac66d8dbfd38
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-10-19 23:25:45 -0400

    Add_Button Methods Integrated

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -50,6 +50,7 @@ class MainWindow():
 
         # button = Button(root, text="Import CSV", command=import_csv)
         # button.pack()
+        self.add_button("Import CSV", import_csv)
 
     def set_window(self, height, width, name, color):
         '''
@@ -75,7 +76,35 @@ class MainWindow():
         '''
         root.mainloop()
     
-
+    def add_button(self, text, command, color="#F7F7F7", font=("Arial", 12), width=10, height=2):
+        '''
+        Adds a button to the window
+        @param text: The text to be displayed on the button
+        @param command: The command to be executed when the button is clicked
+        @param color: The color of the button
+        @param font: The font of the text
+        @param width: The width of the button
+        @param height: The height of the button
+        '''
+        button = Button(root, text=text, command=command, bg=color, font=font, width=width, height=height)
+        button.pack()
+        
+    def add_label(self, text, color="#393E46", font=("Arial", 12)):
+        '''
+        Adds a label to the window
+        @param text: The text to be displayed on the label
+        @param color: The color of the text
+        @param font: The font of the text
+        '''
+        label = Label(root, text=text, bg=color, font=font)
+        label.pack()
+        
+    def add_menu(self, menu, options):
+        '''
+        Adds a menu to the window
+        @param menu: The menu to be added
+        '''
+        return self.menu_bar.add_cascade(label=menu, menu=options)
         
 
 
