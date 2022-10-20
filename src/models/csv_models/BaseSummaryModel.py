2b2ab0dfb176e68ace725bc7ce4e8c815fd74fdf
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-10-20 16:13:39 -0400

    Adjust Models

--- src/models/csv_models/BaseSummaryModel.py
+++ src/models/csv_models/BaseSummaryModel.py
@@ -18,12 +18,12 @@ from dataclasses import dataclass
 @dataclass(order=True)
 class BaseSummaryModel:
     """ BaseSummaryModel For Summary.csv Data """
-    timezone: int = 0
-    UTC_date: datetime = None
-    unix_time_stamp: str = None
-    acc_magnitude_avg: float = None
-    eda_avg: float = None
-    temp_avg: float = None
+    timezone: int
+    UTC_date: str
+    unix_time_stamp: str
+    acc_magnitude_avg: float
+    eda_avg: float
+    temp_avg: float
     movement_intensity: int = 0
     steps_count: int = 0
     rest: int = 0
--- src/models/csv_models/DeviceDataModel.py
+++ src/models/csv_models/DeviceDataModel.py
@@ -16,13 +16,13 @@ from dataclasses import dataclass
 @dataclass(order=True)
 class DeviceDataModel:
     """ DeviceDataModel For DeviceData.csv Data """
-    timezone: int = 0
-    UTC_date: datetime = None
-    firmware_version: str = None
+    timezone: int
+    UTC_date: str
+    firmware_version: str
     app_name: str = "Mate"
-    app_version: str = None
-    mobile_os: str = None
-    mobile_os_version: str = None
-    gtcs_algorithm_version: str = None
+    app_version: str = ""
+    mobile_os: str = ""
+    mobile_os_version: str = ""
+    gtcs_algorithm_version: str = ""
         
         
