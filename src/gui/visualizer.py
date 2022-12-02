4dde88c7eb85e59653ad5814089aa09bc2b29125
SoRRoW.DaDa <59776018+ReaVNaiL@users.noreply.github.com>
2022-12-01 20:38:16 -0500

    Add Visualizer To Startup.py

---
+++ src/gui/visualizer.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5 import QtWidgets
+from gui.MainWindow import UI_MainWindow
+from gui.SecondWindow import UI_SecondWindow
+
+
+class DataVisualizer:
+    def __init__(self) -> None:
+        self.UI = UI_MainWindow()
+            
+    def show(self):
+        app = QtWidgets.QApplication(sys.argv)
+        MainWindow = QtWidgets.QMainWindow()
+        self.UI.setupUi(MainWindow)
+        MainWindow.show()
+        
+        with open("src/gui/style/stylesheet.css", "r") as f:
+            app.setStyleSheet(f.read())
+            
+        # This Adds The Second Window  
+        select_button = self.UI.select_button
+        select_button.clicked.connect(self.open_helper_window)
+        
+        # Import Button
+        self.populate_dropdown()
+        
+        sys.exit(app.exec_())
+
+    def open_helper_window(self):
+        self.print_message()
+        self.window = QtWidgets.QMainWindow()
+        self.ui = UI_SecondWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+    
+    def print_message(self):
+        print("The start date is: ", self.UI.start_date.text())
+        print("The end date is: ", self.UI.end_date.text())
+        print("The index is: ", self.UI.index_dropdown.currentText())
+        print("The subject is: ", self.UI.subject_dropdown.currentText())
+        print("Checkbox is set to: ", self.UI.time_converter_check_box.isChecked())
+        
+    def populate_dropdown(self):
+        # Populate index box:
+        self.UI.index_dropdown.addItem("Summary.csv")
+        self.UI.index_dropdown.addItem("Metadata.csv")
+        
+        # Populate device dropdown
+        self.UI.subject_dropdown.addItem("Apple Watch")
+        self.UI.subject_dropdown.addItem("Fitbit")
+    
+if __name__ == "__main__":
+    ui = DataVisualizer()
+    ui.show()
--- src/startup.py
+++ src/startup.py
@@ -1,27 +1,19 @@
 # Importing the necessary modules
 import os
+from time import sleep
+import asyncio
 
 # Path: Physical-Activity-Tracker-App\src\
-from gui.MainWindow import MainWindow
-import load_csv as lc
-
+from gui.visualizer import DataVisualizer
+from load_csv import load_csv
 
 def start_up():
     # This function will be called when the app is started
     """ Placeholder for main window """
     # # Show the main window
-    program = MainWindow(800, 600, "Physical Activity Tracker")
+    program = DataVisualizer()
     program.show()
-
-    # Need to parse CSV Data into window gui
-    csv_sample_path = os.path.join(os.path.dirname(
-        __file__), "../data/20200118/310/summary.csv")
-
-    csv_object = lc.load_csv(csv_sample_path)
-    # lc.parse_csv(csv_object, "summary")
-
-    print(csv_object[:5])
-
-
+    # program.open_helper_window()
+    
 if __name__ == "__main__":
     start_up()
