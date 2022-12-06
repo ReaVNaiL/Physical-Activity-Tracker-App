5ddebb2c6b291387ce81136c42a640a77159f135
Daniel Bonilla <danielb@zoeller.com>
2022-12-06 11:08:46 -0500

    Add Temp Plotting Handler.py

---
+++ src/PlottingHandler.py
@@ -0,0 +1,17 @@
+from DataHandler import DataHandler
+
+class PlottingHandler:
+    def __init__(self):
+        self._data_handler = DataHandler()
+        self._data_handler.TEST_DATA_HANDLER()
+
+    def TEST_PLOTTING_HANDLER(self):
+        print("TEST_PLOTTING_HANDLER")
+        
+        # for each attribute of data_handler print the value
+        self._data_handler.__repr__()
+
+
+if __name__ == "__main__":
+    plotting_handler = PlottingHandler()
+    plotting_handler.TEST_PLOTTING_HANDLER()
